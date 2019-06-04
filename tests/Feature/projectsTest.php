0006bc83af1b6935322af45620af1694bcc2368c
Monday Victor A <vkingmaker1@gmail.com>
2019-06-04 01:40:06 +0100

    feature(userAuth): add a user Authentication controller
    This commit adds User Authentication feature to the project to make possible the association of a porject to a logged in user
    
    Delivers [#userAuth]

--- tests/Feature/projectsTest.php
+++ tests/Feature/projectsTest.php
@@ -10,13 +10,22 @@ class projectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
+ /** @test */
+
+ public function only_authenticated_users_can_create_project()
+ {
+     $attributes = factory('App\Project')->raw();
+
+     $this->post('/projects', $attributes)->assertRedirect('login');
+ }
+
+
+
 /** @test */
 
  public function a_user_can_create_a_project()
  {
-
-    $this->withoutExceptionHandling();
-
+     $this->actingAs(factory('App\User')->create());
      $attributes = [
          'title' => $this->faker->sentence,
          'description' => $this->faker->paragraph
@@ -29,19 +38,24 @@ class projectsTest extends TestCase
      $this->get('/projects')->assertSee($attributes['title']);
  }
 
+
+
  /** @test */
  public function a_project_requires_a_title()
  {
+    $this->actingAs(factory('App\User')->create());
+
     $attributes = factory('App\Project')->raw(['title'=>'']);
 
     $this->post('/projects',$attributes)->assertSessionHasErrors('title');
  }
 
+
+
  /** @test */
 
  public function a_user_can_view_a_project()
  {
-     $this->withoutExceptionHandling();
      $project = factory('App\Project')->create();
 
      $this->get($project->path())
@@ -49,21 +63,16 @@ class projectsTest extends TestCase
           ->assertSee($project->description);
  }
 
+
+
  /** @test */
 
  public function a_project_requires_a_description()
  {
+     $this->actingAs(factory('App\User')->create());
      $attributes = factory('App\Project')->raw(['description' => '']);
 
      $this->post('/projects', $attributes)->assertSessionHasErrors('description');
  }
 
- /** @test */
-
- public function a_project_requires_an_owner()
- {
-     $attributes = factory('App\Project')->raw(['owner_id' => null ]);
-
-     $this->post('/projects', $attributes)->assertSessionHasErrors('owner_id');
- }
 }
