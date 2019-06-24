938e46530ae2fe94b92de4db08c01bc4c9c78bb2
Monday Victor A <vkingmaker1@gmail.com>
2019-06-24 17:38:01 +0100

    chore(deploy) deploy to aws
    This commit triggers travis to run our tests and deploy the application to AWS
    
    Delivers [#deploy]

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ source "$(pwd)/.env"
 set +o allexport
 
 chmod +x wait-for-it.sh
-./wait-for-it.sh ${DB_HOST}:${DB_PORT} --timeout=60 --strict -- php artisan migrate
+./wait-for-it.sh ${DB_HOST}:${DB_PORT} --timeout=60 --strict -- php artisan migrate --force
 php artisan key:generate
 service php7.2-fpm start && nginx -g "daemon off;"
