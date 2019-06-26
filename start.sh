773e4d0af70cfbeed59846aed4c2bc6b5ca1e803
Monday Victor A <vkingmaker1@gmail.com>
2019-06-26 16:36:10 +0100

    chore(envVariable): remove env file from the source code
    This commit removes the environment variable from the projects source code
    
    Delivers [#env]

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
-set -o allexport
-source "$(pwd)/.env"
-set +o allexport
-
-# chmod +x wait-for-it.sh
-# ./wait-for-it.sh ${DB_HOST}:${DB_PORT} --timeout=60 --strict -- php artisan migrate --force
-php artisan key:generate
+chmod +x wait-for-it.sh
+./wait-for-it.sh ${DB_HOST}:${DB_PORT} --timeout=60 --strict -- php artisan migrate --force
 service php7.2-fpm start && nginx -g "daemon off;"
