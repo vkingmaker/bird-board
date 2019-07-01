912feef918ba35c14fbe2e38ab3fc563cca40415
Monday Victor A <vkingmaker1@gmail.com>
2019-07-01 12:36:20 +0100

    chore(migrate): move the migration command
    this commit refactors the start.sh file by removing the migration command to the bottom of the file so that it doesnt delay execution of other commands in the file
    
    Delivers [#migrate]

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 chmod +x wait-for-it.sh
-./wait-for-it.sh ${DB_HOST}:${DB_PORT} --timeout=60 --strict -- php artisan migrate --force
-# chown -R $USER:www-data storage \
-#     && chown -R $USER:www-data bootstrap/cache \
-#     && chmod -R 775 storage \
-#     && chmod -R 775 bootstrap/cache
 php artisan config:cache && php artisan route:cache
+./wait-for-it.sh ${DB_HOST}:${DB_PORT} --timeout=60 --strict -- php artisan migrate --force
 service php7.2-fpm start && nginx -g "daemon off;"
